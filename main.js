67802aa6ac86b61413d18bb9e51648bf78cc37fd
Alexis Guzman <spartanthelexvro@gmail.com>
2024-07-10 11:38:48 -0600

    Usuario filtrado

--- main.js
+++ main.js
@@ -9,6 +9,13 @@ app.use("/user", (req, res) => {
   });
 });
 
+app.use("user/:id", (req, res) => {
+  const id = req.params;
+  res.json({
+    msg: "Listo",
+  });
+});
+
 app.listener(PORT, () => {
   console.log(`Aplicacion corriendo en http://localhost:${PORT}`);
 });

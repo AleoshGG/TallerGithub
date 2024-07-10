12f7e11949d006b2aae2594c9ed2eb9068902234
Alexis Guzman <spartanthelexvro@gmail.com>
2024-07-10 11:33:32 -0600

    listado de usuarios

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ const app = require("express");
 
 const PORT = 3000;
 
-app.listener(PORT, ()=>{
-    console.log(`Aplicacion corriendo en http://localhost:${PORT}`);
-})
+app.use("/user", (req, res) => {
+  res.json({
+    id: 1,
+    user: "Alexis",
+  });
+});
+
+app.listener(PORT, () => {
+  console.log(`Aplicacion corriendo en http://localhost:${PORT}`);
+});
